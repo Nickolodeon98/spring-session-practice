922ea01b802e5330ad8b860197e2f6554bf6bacf
Nickolodeon98 <sjeon0730@gmail.com>
2023-04-18 22:34:57 +0900

    feat: add a new project

---
+++ src/main/java/com/example/springsessionpractice/SpringSessionPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsessionpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionPracticeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSessionPracticeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/springsessionpractice/SpringSessionPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsessionpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSessionPracticeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
